c33bcbdec47ce38c13e7a04f52cdc0cf986065fe
Ragib Sina <ragib.sina@gmail.com>
2022-12-06 13:21:55 -0700

    added header and about

--- src/container/About/About.js
+++ src/container/About/About.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import { urlFor, client } from "../../client";
+
 import "./About.scss";
 
 const About = () => {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+  return (
+    <>
+      <h2 className="head-text">
+        I Know That <span>Good Apps</span> <br /> means
+        <span> Good Business</span>
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgURL} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
