e5a075a976e51a72fdc794b52d5bbe91dcafbecd
Ragib Sina <ragib.sina@gmail.com>
2022-12-06 11:19:09 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import "./App.scss";
+
 const App = () => {
   return (
     <div className="app">
